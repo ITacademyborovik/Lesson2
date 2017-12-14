adee20e96a643794fb9a2705cafba0864d02fbcb
ITacademyborovik <34532709+ITacademyborovik@users.noreply.github.com>
2017-12-14 11:32:00 +0300

    Add files via upload

---
+++ Lesson2/src/MainApp.java
@@ -0,0 +1,25 @@
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		int i, k = 0;
+		int x1 = 1;
+		int x2 = 2;
+
+		int[] x = new int[5];
+		char y[] = new char[10];
+
+		// Zapolnit massiv dannimi i vivesti ego znachenija na ekran
+
+		for (i = 0; i <= 4; i++) {
+			x[i] = k;
+			k++;
+	}
+
+		for (i = 0; i <= 4; i++) {
+			System.out.println(x[i]);
+		}
+
+	}
+
+}
---
+++ Lesson2/src/Task2.java
@@ -0,0 +1,21 @@
+public class Task2 {
+
+	public static void main(String[] args) {
+		// 1.zapolnit massiv celimi chislami b vivesti soderzhimoe na ekran
+		int x[] = new int[10];
+
+		for (int i = 0; i < x.length; i++) {
+			x[i] = i + 1;
+
+		}
+		for (int i = 0; 1 < x.length; i++) {
+			System.out.println(x[i]);
+		}
+	}
+}
+
+
+//Vivesti iz massiva x v pervom zadanii na ekran v obratnom porjadke
+
+for(i=0; )
+
